9306fdfa1f2c9a9a541797298cced75e2516cff9
Canisa21 <jablonska.paul@gmail.com>
2024-01-27 21:12:20 +0100

    launch_packages_task01-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = galleryItems.map(
+  (galleryItem) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </li>`
+).join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+console.log(galleryItems);
+
+
+gallery.onclick = (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  function onEscape(event) {
+    if (event.code !== "Escape") return;
+    instance.close();
+  }
+
+  const instance = basicLightbox.create(
+    `
+		<img width="1200" src= ${event.target.dataset.source}>
+	`,
+    {
+      onShow: () => window.addEventListener("keydown", onEscape),
+      onClose: () => window.removeEventListener("keydown", onEscape),
+    }
+  );
+  instance.show();
+};
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const createGallery = galleryItems.map(
+  (galleryItem) =>
+    `<li>
+      <a class="gallery__item" href="${galleryItem.original}">
+      <img
+        class="gallery__image"
+        src="${galleryItem.preview}" 
+        alt="${galleryItem.description}"
+        data-source="${galleryItem.original}"
+        >
+        </img>
+        </a>
+     </li>`
+).join("");
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */ captionsData: "alt",
+  captionDelay: 250,
+});
+lightbox.on("show.simplelightbox");
 
 console.log(galleryItems);
+
